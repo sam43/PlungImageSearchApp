d8f854088f884ce4bfc29c043ffda4f9b92a410c
Sayem <asm.sayem@bongobd.com>
2022-01-21 01:16:22 +0600

    Hilt-DI impl - App.kt & AppModule.kt added for hilt (network call)

---
+++ app/src/main/java/com/plung/imagesearchapp/di/AppModule.kt
@@ -0,0 +1,130 @@
+package com.plung.imagesearchapp.di
+
+import com.plung.imagesearchapp.BuildConfig.DEBUG
+import com.plung.imagesearchapp.api.UnsplashApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.logging.HttpLoggingInterceptor
+import okio.Buffer
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.nio.charset.Charset
+import java.util.concurrent.TimeUnit
+import javax.inject.Named
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+    @Provides
+    @Singleton
+    fun provideRetrofit(client: OkHttpClient): Retrofit = Retrofit.Builder().baseUrl(UnsplashApi.BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .client(client)
+        .build()
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(
+        @Named("HeaderInterceptor") headerInterceptor: Interceptor,
+        @Named("CurlInterceptor") curlInterceptor: Interceptor,
+        loggerInterceptor: HttpLoggingInterceptor
+    ): OkHttpClient {
+        val okHttpClientBuilder = OkHttpClient().newBuilder()
+        okHttpClientBuilder.connectTimeout(Constants.CONNECTION_TIMEOUT.toLong(), TimeUnit.SECONDS)
+        okHttpClientBuilder.readTimeout(Constants.READ_TIMEOUT.toLong(), TimeUnit.SECONDS)
+        okHttpClientBuilder.writeTimeout(Constants.WRITE_TIMEOUT.toLong(), TimeUnit.SECONDS)
+        okHttpClientBuilder.addInterceptor(headerInterceptor)
+        if (DEBUG) {
+            okHttpClientBuilder.addInterceptor(loggerInterceptor)
+            okHttpClientBuilder.addInterceptor(curlInterceptor)
+        }
+        return okHttpClientBuilder.build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideUnsplashApi(retrofit: Retrofit): UnsplashApi =
+        retrofit.create(UnsplashApi::class.java)
+
+    @Provides
+    @Singleton
+    @Named("HeaderInterceptor")
+    fun provideHeaderInterceptor(): Interceptor = Interceptor {
+        val requestBuilder = it.request().newBuilder()
+        //hear you can add all headers you want by calling 'requestBuilder.addHeader(name ,  value)'
+        it.proceed(requestBuilder
+            .build())
+    }
+
+    @Provides
+    @Singleton
+    fun provideLoggerInterceptor(): HttpLoggingInterceptor = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+
+    @Provides
+    @Singleton
+    @Named("CurlInterceptor")
+    fun provideCurlInterceptor(logger: HttpLoggingInterceptor.Logger): Interceptor = Interceptor { chain ->
+        val uft8 = Charset.forName("UTF-8")
+        val request: Request = chain.request()
+        var compressed = false
+        val curlCmdBuilder = StringBuilder()
+        curlCmdBuilder.append("curl ")
+        curlCmdBuilder.append("-X ")
+        curlCmdBuilder.append(request.method)
+        val headers = request.headers
+        var i = 0
+        val count = headers.size
+        while (i < count) {
+            val name = headers.name(i)
+            val value = headers.value(i)
+            if ("Accept-Encoding".equals(name, ignoreCase = true) && "gzip".equals(
+                    value,
+                    ignoreCase = true
+                )
+            ) {
+                compressed = true
+            }
+            curlCmdBuilder.append(" -H ")
+            curlCmdBuilder.append("\"")
+            curlCmdBuilder.append(name)
+            curlCmdBuilder.append(": ")
+            curlCmdBuilder.append(value)
+            curlCmdBuilder.append("\"")
+            i++
+        }
+        val requestBody = request.body
+        if (requestBody != null) {
+            val buffer = Buffer()
+            requestBody.writeTo(buffer)
+            var charset = uft8
+            val contentType = requestBody.contentType()
+            if (contentType != null) {
+                charset = contentType.charset(uft8)
+            }
+            // try to keep to a single line and use a subshell to preserve any line breaks
+            curlCmdBuilder.append(" --data $'")
+            curlCmdBuilder.append(buffer.readString(charset!!).replace("\n", "\\n"))
+            curlCmdBuilder.append("'")
+        }
+        curlCmdBuilder.append(if (compressed) " --compressed " else " ")
+        curlCmdBuilder.append("\"")
+        curlCmdBuilder.append(request.url)
+        curlCmdBuilder.append("\"")
+        logger.log("╭--- cURL (" + request.url + ")")
+        logger.log(curlCmdBuilder.toString())
+        logger.log("╰--- (copy and paste the above line to a terminal)")
+        chain.proceed(request)
+    }
+
+
+    @Provides
+    @Singleton
+    fun provideLogger() = HttpLoggingInterceptor.Logger.DEFAULT
+
+}
---
+++ app/src/main/java/com/plung/imagesearchapp/di/Constants.kt
@@ -0,0 +1,10 @@
+package com.plung.imagesearchapp.di
+
+object Constants {
+    // DI helper
+    const val TAG: String = "HiltClass"
+    const val READ_TIMEOUT = 30
+    const val WRITE_TIMEOUT = 30
+    const val CONNECTION_TIMEOUT = 10
+    const val CACHE_SIZE_BYTES = 10 * 1024 * 1024L // 10 MB
+}
